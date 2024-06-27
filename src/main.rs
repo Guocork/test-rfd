0a828d9dd5f4adfa95a8f227f4e8133f8fcb81bf
Guocork <g1024536444@gmail.com>
2024-06-27 23:01:52 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::fs;
+
 use rfd::FileDialog;
 
 fn main() {
     if let Some(path) = FileDialog::new().pick_folder() {
         println!("Selected folder: {:?}", path);
+        fs::create_dir_all(&path).unwrap_or_else(|err| {
+            eprintln!("Error creating new download directory: {:?}", err);
+        });
     } else {
         println!("No folder selected");
     }
